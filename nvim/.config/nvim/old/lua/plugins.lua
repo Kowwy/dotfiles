bc3b0ffc8876d151e0cd9876926aeb1eb01bbe3f
Corey Glover <coreyglover@live.com>
2022-04-17 21:15:32 -0500

    restructuring init.lua

---
+++ nvim/.config/nvim/old/lua/plugins.lua
@@ -0,0 +1,74 @@
+vim.cmd([[
+    augroup packer_user_config
+		autocmd!
+		autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+]])
+
+function get_setup(name)
+    return string.format('require("setup/%s")', name)
+end
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-treesitter/nvim-treesitter', 
+		config = get_setup('treesitter'), 
+		run = ':TSUpdate'
+	}
+	use 'p00f/nvim-ts-rainbow'
+	use 'kyazdani42/nvim-web-devicons'
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {
+		'nvim-lualine/lualine.nvim',
+		config = get_setup('lualine'),
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	use {
+		'catppuccin/nvim',
+		as = 'catppuccin',
+		config = get_setup('catppuccin')
+	}
+	use 'nathom/filetype.nvim'
+	use {
+		'alvarosevilla95/luatab.nvim',
+		requires = 'kyazdani42/nvim-web-devicons',
+		config = get_setup('luatab')
+	}
+	use {
+		'neovim/nvim-lspconfig',
+		config = get_setup('lspconfig')
+	}
+	use {
+		'williamboman/nvim-lsp-installer',
+		config = get_setup('lsp-installer')
+	}
+	use {
+		'folke/which-key.nvim',
+		config = get_setup('which-key')
+	}
+	use {
+		'windwp/nvim-autopairs',
+		config = get_setup('autopairs')
+--		after = 'nvim-cmp'
+	}
+	use {
+		'hrsh7th/nvim-cmp',
+		requires = {
+			'hrsh7th/cmp-cmdline',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-nvim-lsp-signature-help',
+			'hrsh7th/vim-vsnip',
+			'hrsh7th/cmp-vsnip'
+		},
+		config = get_setup('autocomplete')
+	}
+	use 'onsails/lspkind-nvim'
+
+
+end)
